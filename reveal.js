473949d59e7cad902a5867961aa3bb1171757bfe
walsinaw <juliawalsinaw@gmail.com>
2026-05-22 16:53:14 -0300

    Adição de animação em títulos iniciais

---
+++ reveal.js
@@ -0,0 +1,12 @@
+window.revelar = ScrollReveal({reset: false})
+
+revelar.reveal('.efeito-titulo', {
+    duration: 2000,
+    distance: '90px',
+})
+
+revelar.reveal('.efeito-descricao', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})

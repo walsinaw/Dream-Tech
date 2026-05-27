137b701cd470af440d350b0de882af74ef006ae6
walsinaw <juliawalsinaw@gmail.com>
2026-05-27 14:34:45 -0300

    Ajuste no mobile menu

--- menu.js
+++ menu.js
@@ -2,19 +2,25 @@ const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const overlay = document.querySelector('.menu-overlay');
 
-    function toggleMenu() {
-        mobileMenu.classList.toggle('active');
-        overlay.classList.toggle('active');
-        document.body.style.overflow = mobileMenu.classList.contains('active') ? 'hidden' : '';
-        }
+function toggleMenu() {
+    mobileMenu.classList.toggle('active');
+    overlay.classList.toggle('active');
+    document.body.style.overflow = mobileMenu.classList.contains('active') ? 'hidden' : '';
+    }
 
-    if (hamburger) {
-            hamburger.addEventListener('click', toggleMenu);
-        }
+if (hamburger) {
+        hamburger.addEventListener('click', toggleMenu);
+    }
 
-    if (overlay) {
-            overlay.addEventListener('click', toggleMenu);
+if (overlay) {
+        overlay.addEventListener('click', toggleMenu);
+}
+
+const menuClose = document.querySelector('.menu-close');
+
+if (menuClose) {
+    menuClose.addEventListener('click', toggleMenu);
 }
 
 document.querySelectorAll('.mobile-nav a').forEach(link => {
-    link.addEventListener('click', toggleMenu);});
+link.addEventListener('click', toggleMenu);});
